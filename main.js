d6277ef6dbb9a1245e7627d5a8f6e0eaeff95c46
ZOG-WEB <79850764+ZOG-WEB@users.noreply.github.com>
2021-08-06 19:05:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+var mouseEvent="empty";
+var last_position_of_x,last_position_of_y;
+canvas=document.getElementById('MyCanvas'); 
+ctx=canvas.getContext("2d");
+color="grey";
+width_of_line=1;
+canavs.addEventListener("mousedown", my_mousedown);
+function my_mousedown(){
+    color=document.getElementById("color").value;
+    width_of_line=document.getElementById("width_of_line").value;
+    mouseEvent="mouseDown";
+}
+canvas=addEventListener("mousemove", my_mousemove);
+function my_mousemove(e){
+    current_position_of_mouse_x=e.clientX-canvas.offsetLeft;
+    current_position_of_mouse_y=e.clientY-canvas.offsetTop;
+    if(mouseEvent=="mouseDown"){
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=width_of_line;
+console.log("last position of x and y coordinates=");
+console.log("x="+last_position_of_x+"y="+last_position_of_y );
+ctx.moveTo(last_position_of_x+last_position_of_y);
+console.log("current position of x and y coordinates=");
+console.log("x="+current_position_of_x+"y="+current_position_of_y );
+ctx.moveTo(current_position_of_x+current_position_of_y);
+ctx.stroke();
+    }
+    last_position_of_x=current_position_of_mouse_x;
+    last_position_of_y=current_position_of_mouse_y;
+}
+canvas.addEventListener("mouseup", my_mouseup); 
+function my_mouseup(e)
+ {
+      mouseEvent = "mouseUP";
+    }
+    canvas.addEventListener("mouseleave", my_mouseleave);
+    function my_mouseleave(e) 
+    { 
+        mouseEvent = "mouseleave"; 
+    
+    
+    }  
+
+
+
+
+
+
+
+
+
+
+
+
